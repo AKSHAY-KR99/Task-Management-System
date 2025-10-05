525d8a74343903e42507718033513c7903914da0
AKSHAY-KR99 <akshayakr4@gmail.com>
2025-10-06 00:00:33 +0530

    template path correction

--- task_management_system/settings.py
+++ task_management_system/settings.py
@@ -40,7 +40,7 @@ ROOT_URLCONF = 'task_management_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
